621af6af0a7f0b56da8a444fdd2f7597cf96fb66
jacek <jacek@example.pl>
2019-02-07 10:25:28 +0100

    pylint update

--- network2.py
+++ network2.py
@@ -1,24 +1,24 @@
 """Moduł zawiera funkcje do rysowania grafu i tworzenia tabeli"""
 
-import networkx as nx
+from math import sqrt
 from itertools import combinations
+import networkx as nx
 import plotly
 import plotly.graph_objs as go
 import dash_table
-from math import sqrt
 
 
 def draw_network(data=None, authors=None, units=None):
-    print('Creating graph...')
-    G = nx.Graph()
-    print(authors)
-    print(units)
+    """Funkcja na podstawie danych oraz podanych ograniczeń w postacji autorów i jednostek
+    do których są przyporządkowani rysuje graf połączeń."""
 
+    print('Creating graph...')
+    authors_graph = nx.Graph()
 
     papers = []
     for author, values in  data['authors'].items():
         if (not authors or author in authors) and (not units or (set(values['units'])&set(units))):
-            G.add_node(author, n_publications=len(values['papers']))
+            authors_graph.add_node(author, n_publications=len(values['papers']))
             papers.extend(values['papers'])
 
     for paper in papers:
@@ -32,18 +32,18 @@ def draw_network(data=None, authors=None, units=None):
         for pair in ed:
             n_common_papers = len(set(data['authors'][pair[0]]['papers']) &
                                   set(data['authors'][pair[1]]['papers']))
-            G.add_edge(*pair, weight=n_common_papers)
+            authors_graph.add_edge(*pair, weight=n_common_papers)
 
-            G.add_node(pair[0], n_publications=len(data['authors'][pair[0]]['papers']))
-            G.add_node(pair[1], n_publications=len(data['authors'][pair[1]]['papers']))
+            authors_graph.add_node(pair[0], n_publications=len(data['authors'][pair[0]]['papers']))
+            authors_graph.add_node(pair[1], n_publications=len(data['authors'][pair[1]]['papers']))
 
 
 
-    p = nx.spring_layout(G)
-    nx.set_node_attributes(G, p, name='pos')
+    p = nx.spring_layout(authors_graph)
+    nx.set_node_attributes(authors_graph, p, name='pos')
 
     edges_trace = []
-    for edge in G.edges():
+    for edge in authors_graph.edges():
         edge_trace = go.Scatter(
             x=[],
             y=[],
@@ -51,11 +51,11 @@ def draw_network(data=None, authors=None, units=None):
             hoverinfo='none',
             mode='lines')
 
-        x0, y0 = G.node[edge[0]]['pos']
-        x1, y1 = G.node[edge[1]]['pos']
+        x0, y0 = authors_graph.node[edge[0]]['pos']
+        x1, y1 = authors_graph.node[edge[1]]['pos']
         edge_trace['x'] += tuple([x0, x1, None])
         edge_trace['y'] += tuple([y0, y1, None])
-        edge_trace['line']['width'] = G[edge[0]][edge[1]]['weight']
+        edge_trace['line']['width'] = authors_graph[edge[0]][edge[1]]['weight']
 
         edges_trace.append(edge_trace)
 
@@ -80,15 +80,15 @@ def draw_network(data=None, authors=None, units=None):
             line=dict(width=2, color='black'))
         )
 
-    for node in G.nodes():
-        x, y = G.node[node]['pos']
+    for node in authors_graph.nodes():
+        x, y = authors_graph.node[node]['pos']
         node_trace['x'] += tuple([x])
         node_trace['y'] += tuple([y])
 
-    for node in G.nodes():
-        node_trace['marker']['color'] += tuple([G.node[node]['n_publications']])
-        node_trace['marker']['size'] += tuple([sqrt(len(G[node]) + 1) * 10])  # liczba sąsiadów
-        node_info = str(node) + ': liczba publikacji: '+str(G.node[node]['n_publications'])
+    for node in authors_graph.nodes():
+        node_trace['marker']['color'] += tuple([authors_graph.node[node]['n_publications']])
+        node_trace['marker']['size'] += tuple([sqrt(len(authors_graph[node]) + 1) * 10])  # liczba sąsiadów
+        node_info = str(node) + ': liczba publikacji: '+str(authors_graph.node[node]['n_publications'])
         node_trace['text'] += tuple([node_info])
 
     figure_data = edges_trace + [node_trace]
@@ -112,7 +112,9 @@ def draw_network(data=None, authors=None, units=None):
     return fig
 
 
-def authors_table(data, authors, units):
+def authors_table(data, authors=None, units=None):
+    """Zwraca tablę z publikacjami danych autorów"""
+
     authors_info = data['authors']
     children = []
     if authors or units:
@@ -155,6 +157,8 @@ def authors_table(data, authors, units):
 
 
 def acronym(units):
+    """Zwraca skróconą nazwę jednostki"""
+
     units_list = []
     for unit in units:
         units_list.append(''.join(word[0] for word in unit.split()))
@@ -162,5 +166,7 @@ def acronym(units):
 
 
 def trim_title(title, size=120):
+    """Przycina zbyt długie nazwy publikacji"""
+
     ellipsis = '...'
     return (title[:size] + ellipsis) if len(title) > size + len(ellipsis) else title
